1f3769fd062396b79ae4a26977a45d4277429ea5
cjkunselman18 <53878004+cjkunselman18@users.noreply.github.com>
2019-08-13 10:28:34 -0500

    Update Export_for_S4VM.py

--- Export_for_S4VM.py
+++ Export_for_S4VM.py
@@ -11,7 +11,7 @@ worksheet.write(0,2, 'PC3')
 
 row = 1
 
-for col, data in enumerate(processed_no_test.T):
+for col, data in enumerate(processed.T):
     worksheet.write_column(row, col, data)
 
 
@@ -21,7 +21,7 @@ workbook = xlsxwriter.Workbook('Processed Autocorrelation Data Labels.xlsx')
 worksheet = workbook.add_worksheet()
 
 worksheet.write(0,0, 'labels')
-worksheet.write_column(1,0, np.array(label_list_train).T)
+worksheet.write_column(1,0, np.array(label_list_train + label_list_test).T)
 
 
 workbook.close()
