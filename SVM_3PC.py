738633d0a304092072711183ac73d8c33761d26d
cjkunselman18 <53878004+cjkunselman18@users.noreply.github.com>
2019-08-13 11:22:07 -0500

    Update SVM_3PC.py

--- SVM_3PC.py
+++ SVM_3PC.py
@@ -5,24 +5,24 @@
 print("# Tuning hyper-parameters for accuracy")
 print()
 
-clf = GridSearchCV(SVC(probability = True), tuned_parameters, cv=5,
+clf3 = GridSearchCV(SVC(probability = True), tuned_parameters, cv=5,
                        scoring='accuracy' )
-clf.fit(processed_train[:,0:3], label_list_train)
+clf3.fit(processed_train[:,0:3], label_list_train)
 
 print("Best parameters set found on development set:")
 print()
-print(clf.best_params_)
+print(clf3.best_params_)
 print()
 print("Grid scores on development set:")
 print()
-means = clf.cv_results_['mean_test_score']
-stds = clf.cv_results_['std_test_score']
-for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+means = clf3.cv_results_['mean_test_score']
+stds = clf3.cv_results_['std_test_score']
+for mean, std, params in zip(means, stds, clf3.cv_results_['params']):
     print("%0.3f (+/-%0.03f) for %r"
             % (mean, std * 2, params))
 
 # Test accuracy
-baseline_svm = clf.best_estimator_
+baseline_svm = clf3.best_estimator_
 predicted_test_labels_3D = baseline_svm.predict(processed_test[:,0:3])
 mat = confusion_matrix(label_list_test, predicted_test_labels_3D)
 plt.figure(0)
