9fe5192d7e3087531d7cf68ec27e2f767ee914d4
cjkunselman18 <53878004+cjkunselman18@users.noreply.github.com>
2019-08-13 10:33:45 -0500

    Update Matlab_S4VM.m

--- Matlab_S4VM.m
+++ Matlab_S4VM.m
@@ -9,8 +9,8 @@ labels = table2array(labels);
 
 % get inputs from read-in data
 X_train = data(1:153,:);
-X_unknown = data(154:205,:);
-labels_train = labels;
+X_unknown = data(193:244,:);
+labels_train = labels(1:153);
 
 % parameters - C1 is from the baseline SVM, and the rest are defaults
 C1 = 10;
@@ -18,13 +18,20 @@ C2 = 0.1;
 gamma = 0;
 sampleTime = 100;
 
-% run S4VM; "prediction" is the predicted labels for the unlabeled set
+% run S4VM; "prediction_unlabeled" is the predicted labels for the unlabeled set
 % the linear kernel is from the baseline SVM
 addpath('libsvm-mat-2.89-3-box constraint');
-prediction=S4VM(X_train,labels_train,X_unknown,'Linear',C1,C2,sampleTime,gamma);
+prediction_unlabeled = S4VM(X_train,labels_train,X_unknown,'Linear',C1,C2,sampleTime,gamma);
+
+% check the first criterion - that the S4VM method labels the test set correctly
+X_test = data(154:192,:);
+prediction_test = S4VM(X_train,labels_train,X_test,'Linear',C1,C2,sampleTime,gamma);
+
+% if this is true, the method passes
+max(abs(prediction_test - labels(154:192))) == 0
 
 
 % write to new excel document so we can load these results back into python
-prediction_table = array2table(prediction)
+prediction_table = array2table(prediction_unlabeled)
 filename = 'S4VM_Prediction.xlsx'
 writetable(prediction_table,filename)
