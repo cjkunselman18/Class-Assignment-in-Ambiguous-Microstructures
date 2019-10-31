ca14c76df73743a1988ec8a1b9f41a3ce663fcaa
cjkunselman18 <53878004+cjkunselman18@users.noreply.github.com>
2019-10-31 15:15:31 -0500

    Update Modified_Yarowsky.py

--- Modified_Yarowsky.py
+++ Modified_Yarowsky.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
 
-# find initial labels for first iteration
+# find labels for first iteration
 unknown_labels = baseline_svm.predict(processed_unknown)
 
-# update the training label list
 label_list_retrain = label_list_train + list(unknown_labels)
 
 # and append the training set
-processed_no_test = np.ones((205,3))
+processed_no_test = np.ones((205,50))
 processed_no_test[0:153,:] = processed_train
 processed_no_test[153:205,:] = processed_unknown
 
-# create a new classification object with the same kernel/hyperparameters, and retrain
-svm_retrain = clf3.best_estimator_
+# same parameters as the baseline SVM
+svm_retrain = SVC(C=1,gamma=0.01,kernel='rbf')
 svm_retrain.fit(processed_no_test, label_list_retrain)
 
 # make a new prediction on the initially unlabeled samples
@@ -23,10 +22,10 @@ convergence_check = np.amax(abs(unknown_labels - unknown_labels_again))==0
 iterations = 1
 
 # if not done, this loop will run until convergence is reached
-while convergence_check == False and iterations < 101 :
+while convergence_check == False:
     unknown_labels = unknown_labels_again[:]
     label_list_retrain = label_list_train + list(unknown_labels)
-    svm_retrain = clf3.best_estimator_
+    svm_retrain = SVC(C=1,gamma=0.01,kernel='rbf')
     svm_retrain.fit(processed_no_test, label_list_retrain)
     unknown_labels_again = svm_retrain.predict(processed_unknown)
     convergence_check = np.amax(abs(unknown_labels - unknown_labels_again))==0
