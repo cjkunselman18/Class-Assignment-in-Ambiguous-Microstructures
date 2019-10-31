6c147befce31fbe5821a97ba70ad49625e2f2b6e
cjkunselman18 <53878004+cjkunselman18@users.noreply.github.com>
2019-10-30 20:12:52 -0500

    Update and rename Characterization_Black_Autocorrelation.py to Characterization_Normalized_Black_Autocorrelation.py

--- Characterization_Normalized_Black_Autocorrelation.py
+++ Characterization_Normalized_Black_Autocorrelation.py
@@ -29,3 +29,21 @@ for i in range(0,52):
 # Uncomment the line below to draw an autocorrelation. Only change the first index to move down the list
 # draw_autocorrelations(two_point_correlations_precipitate[0][0],autocorrelations=[(0,0)])
 
+
+# Now we are going to calculate black phase volume fraction and normalize the two-point correlations
+vol_frac_precipitate = []
+vol_frac_bicontinuous = []
+vol_frac_unknown = []
+
+for i in range(0,144):
+    vol_frac_precipitate.append(1-sum(sum(binary_image_list_precipitate[i]))/262144)
+    two_point_correlations_precipitate[i] = (two_point_correlations_precipitate[i] - vol_frac_precipitate[i]**2)/(vol_frac_precipitate[i] - vol_frac_precipitate[i]**2)
+    
+for i in range(0,48):
+    vol_frac_bicontinuous.append(1-sum(sum(binary_image_list_bicontinuous[i]))/262144)
+    two_point_correlations_bicontinuous[i] = (two_point_correlations_bicontinuous[i] - vol_frac_bicontinuous[i]**2)/(vol_frac_bicontinuous[i] - vol_frac_bicontinuous[i]**2)
+    
+for i in range(0,52):
+    vol_frac_unknown.append(1-sum(sum(binary_image_list_unknown[i]))/262144)
+    two_point_correlations_unknown[i] = (two_point_correlations_unknown[i] - vol_frac_unknown[i]**2)/(vol_frac_unknown[i] - vol_frac_unknown[i]**2)
+
